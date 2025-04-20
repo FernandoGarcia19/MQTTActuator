1062ebb88850cd46088c3fb21d3ce56a82e6a42a
FernandoGarcia19 <fernando.garcia@ucb.edu.bo>
2025-04-20 11:34:02 -0400

    First commit

---
+++ ServoClient.h
@@ -0,0 +1,44 @@
+#ifndef SERVOCLIENT_H
+#define SERVOCLIENT_H
+#include <ESP32Servo.h>
+
+class ServoClient {
+  private:
+    Servo servo;
+    byte pin;
+    byte currentAngle;
+    byte targetAngle;
+    long delayDuration;
+  public:
+    ServoClient(byte pin, byte initialAngle, long delayDuration)
+    : pin(pin), currentAngle(initialAngle), targetAngle(initialAngle), delayDuration(delayDuration){
+    }
+    void init(){
+      servo.attach(pin);
+      servo.write(targetAngle);
+    }
+    void moveToTarget(){
+      servo.write(currentAngle);
+      delay(delayDuration);
+    }
+    void loop(){
+      if (currentAngle < targetAngle){
+        currentAngle++;
+      }
+      else if (currentAngle > targetAngle){
+        currentAngle--;
+      }
+      moveToTarget();
+    }
+    void setTarget(const byte& target){
+      this->targetAngle = target;
+    }
+    void open(){
+      targetAngle = 90; 
+    }
+    void close(){
+      targetAngle = 0;
+    }
+};
+
+#endif 
